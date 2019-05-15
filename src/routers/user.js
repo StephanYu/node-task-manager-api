b1e20b5f2cc0500faaefedc99c9ec5c9d4125238
Stephan Yu <Stephan.yu@gmail.com>
2019-05-15 13:35:54 +0100

    Implement file upload feature
    Add endpoints for uploading, deleting, and showing user profile image
    Create validations for file uploads
    Handle Express errors
    Use sharp for image formatting and cropping
    Use multer to handle file uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const sharp = require('sharp')
 const User = require('../models/user')
-const router = express.Router()
 const auth = require('../middleware/authentication')
+const multer = require('multer')
+
+const router = express.Router()
 
 router.post('/users', async (req, res) => {
 	const user = new User(req.body)
@@ -88,5 +91,50 @@ router.delete('/users/myprofile', auth, async (req, res) => {
 		res.status(500).send(error)
 	}
 })
+
+const uploadAvatar = multer({
+	limits: {
+		fileSize: 1000000
+	},
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+			return cb(new Error('Please upload either a .jpg or .png formatted image file.'))
+		}
+
+		cb(undefined, true)
+	}
+})
+router.post('/users/myprofile/avatar', auth, uploadAvatar.single('avatar'), async (req, res) => {
+	const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+	req.user.avatar = buffer
+	await req.user.save()
+
+	res.send()
+}, (error, req, res, next) => {
+	res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/myprofile/avatar', auth, async (req, res) => {
+	req.user.avatar = undefined
+	await req.user.save()
+
+	res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id)
+
+		if (!user || !user.avatar) {
+			throw new Error()
+		}
+
+		res.set('Content-Type', 'image/png')
+		res.send(user.avatar)
+	} catch (error) {
+		res.status(404).send()
+	}
+})
+
 
 module.exports = router
